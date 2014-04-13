9d21be71227f3087dfb3678d6c2003f1790ce06d
Charles Strahan <charles.c.strahan@gmail.com>
2014-04-13 05:37:40 -0400

    use infix

--- happybara/src/Happybara/Monad.hs
+++ happybara/src/Happybara/Monad.hs
@@ -289,7 +289,7 @@ synchronize act = do
     retry startTime maxWait =
         act `catch` (\(e :: InvalidElementException) -> do
             currenTime <- liftBase getCurrentTime
-            let elapsedSeconds = fromRational $ toRational $ diffUTCTime currenTime startTime
+            let elapsedSeconds = fromRational $ toRational $ currenTime `diffUTCTime` startTime
             if elapsedSeconds > maxWait
               then throw e
               else do
